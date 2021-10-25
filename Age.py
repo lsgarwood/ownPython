1beaf362d7355fdf88f4e706b441ec8381a9855e
Lauren Garwood <ls_garwood@yahoo.co.uk>
2021-10-25 13:22:34 +0100

    commiting my personal pyhton projects

---
+++ Age.py
@@ -0,0 +1,3 @@
+age=input("How old are you?")
+ageNextYear=int(age)+1
+print("You will be", ageNextYear, "on your next birthday.")
---
+++ doyoulikerobots.py
@@ -0,0 +1,11 @@
+user_reply=input("Do you like robots? (Type yes or no)")
+if user_reply == "yes":
+    print("Beep boop!")
+elif user_reply == "maybe":
+    print("Make up your mind, human.")
+elif user_reply =="only ones with laser eyes":
+    print("Zzzzap!")
+elif user_reply == "no":
+    print("Well, robotos don't like you either.")
+else:
+    print("Your human nonsense offends us.")
